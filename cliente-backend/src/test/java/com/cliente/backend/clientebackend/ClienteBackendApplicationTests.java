19a909b30ff345fb1dd086c95fbe9c6319b16808
juanvaleriomayta <jvaleriom@vallegrande.edu.pe>
2021-10-14 23:22:50 -0500

    comenzando con la creación de la factura en Udemy

---
+++ cliente-backend/src/test/java/com/cliente/backend/clientebackend/ClienteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.backend.clientebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cliente-frontend/src/app/clientes/cliente.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from './cliente';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  private urlEndPoint: string = 'http://localhost:9999/api/clientes';
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+    //return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
+  }
+}
